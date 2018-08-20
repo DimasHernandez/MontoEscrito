d412a19404dfc88f356394208ab5ac815a43472e
Dimas <dimas.hernandez@utp.edu.co>
2018-08-20 02:04:25 -0500

    Creacion segundo metodo (centenas) con su respectivo Test.

--- src/PaqueteMontoEscrito/MontoEscrito.java
+++ src/PaqueteMontoEscrito/MontoEscrito.java
@@ -3,7 +3,8 @@ package PaqueteMontoEscrito;
 //Creacion de clase.
 public class MontoEscrito {
 
-//Creacion del metodo.							//un string es un arreglo, es una variable abstracta.
+//Creacion del metodo.							
+//un string es un arreglo, es una variable abstracta.
 	public String ConvertirNumero(String valor) {
 
 		String valorletras = "";
@@ -17,36 +18,68 @@ public class MontoEscrito {
 			valorletras = "Mil";
 			break;
 		case "2":
-			valorletras = " Dos mil";
+			valorletras = "Dos mil";
 			break;
 		case "3":
-			valorletras = " Tres mil";
+			valorletras = "Tres mil";
 			break;
 		case "4":
-			valorletras = " Cuatro mil";
+			valorletras = "Cuatro mil";
 			break;
 		case "5":
-			valorletras = " Cinco mil ";
+			valorletras = "Cinco mil";
 			break;
 		case "6":
-			valorletras = " Seis mil ";
+			valorletras = "Seis mil";
 			break;
 		case "7":
-			valorletras = " Siete mil ";
+			valorletras = "Siete mil";
 			break;
 		case "8":
-			valorletras = " Ocho mil ";
+			valorletras = "Ocho mil";
 			break;
 		case "9":
-			valorletras = " Nueve mil ";
+			valorletras = "Nueve mil";
 			break;
 
 		}
 
-		
+		// Switch para las Centenas.
+		switch (valor.substring(1, 2)) {
+		case "1":
+			if (valor.charAt(2) == '0' && valor.charAt(3) == '0') {
+				
+				valorletras = " Cien ";
+			} else {
+				valorletras = " Ciento ";
+			}break;
 
-		
+		case "2":
+			valorletras = " Doscientos ";
+			break;
+		case "3":
+			valorletras = " Trescientos ";
+			break;
+		case "4":
+			valorletras = " Cuatrocientos ";
+			break;
+		case "5":
+			valorletras = " Quinientos ";
+			break;
+		case "6":
+			valorletras = " Seiscientos ";
+			break;
+		case "7":
+			valorletras = " Setecientos ";
+			break;
+		case "8":
+			valorletras = " Ochocientos ";
+			break;
+		case "9":
+			valorletras = " Novecientos ";
+			break;
 
+		}
 		return valorletras;
 	}
-
+}
--- src/Test/MontoEscritoTest.java
+++ src/Test/MontoEscritoTest.java
@@ -13,12 +13,26 @@ class MontoEscritoTest {
 		// Instanciar la clase MontoEscrito
 		MontoEscrito PruebaMIles = new MontoEscrito();
 		// declaro variable valor esperado
-		String letrasesperadas = "Mil";
+		String letrasesperadas = "Nueve mil";
 		// declaro variable valor obtenido
-		String letrasobtenidas = PruebaMIles.ConvertirNumero("1000");
+		String letrasobtenidas = PruebaMIles.ConvertirNumero("9000");
 		// se hace la comparación
+		//System.out.println(letrasobtenidas);
+		assertEquals(letrasesperadas, letrasobtenidas);
+	}
+
+	@Test
+	void TestCentenas() {
+		// Instanciar la clase MontoEscrito.
+		MontoEscrito PruebaCentenas = new MontoEscrito();
+		// Declaro variable valor esperado.
+		String letrasesperadas = " Ciento ";
+		// Declaro variable valor obtenido.
+		String letrasobtenidas = PruebaCentenas.ConvertirNumero(" 199 ");
+		// Hago la comparacion.
 		System.out.println(letrasobtenidas);
 		assertEquals(letrasesperadas, letrasobtenidas);
+
 	}
 
 }
